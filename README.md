1fadbcd78ce4e1a8704213588d303751b5c8d02e
Bruno Molteni <brunomolteni90@gmail.com>
2019-07-05 19:48:31 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# 🔃 SortableList
+
+This component renders a list of items which can be reordered by draggin and dropping and implements FLIP animation for adding/removing/reordering items in the list.
+
+## ⤵️ Props
+
+| name       | type      | required | default |
+| ---------- | --------- | -------- | ------- |
+| `list`     | array     | ✔️       | ✖️      |
+| `listItèm` | component | ❌       |
+
+## ⤴️ Events
---
+++ SortableList.svelte
@@ -0,0 +1,93 @@
+<script>
+  import { quintOut } from "svelte/easing";
+  import { crossfade } from "svelte/transition";
+  import { flip } from "svelte/animate";
+
+  // FLIP ANIMATION
+  const [send, receive] = crossfade({
+    duration: d => Math.sqrt(d * 200),
+
+    fallback(node, params) {
+      const style = getComputedStyle(node);
+      const transform = style.transform === "none" ? "" : style.transform;
+
+      return {
+        duration: 600,
+        easing: quintOut,
+        css: t => `
+					transform: ${transform} scale(${t});
+					opacity: ${t}
+				`
+      };
+    }
+  });
+
+  // DRAG AND DROP
+  let isOver = false;
+  const getDraggedParent = node =>
+    (node.dataset.index && node.dataset) || getDraggedParent(node.parentNode);
+  const start = ev => {
+    ev.dataTransfer.setData("source", ev.target.dataset.index);
+  };
+  const over = ev => {
+    ev.preventDefault();
+    let dragged = getDraggedParent(ev.target);
+    if (isOver !== dragged.id) isOver = JSON.parse(dragged.id);
+  };
+  const leave = ev => {
+    let dragged = getDraggedParent(ev.target);
+    if (isOver === dragged.id) isOver = false;
+  };
+  const drop = ev => {
+    isOver = false;
+    ev.preventDefault();
+    let dragged = getDraggedParent(ev.target);
+    let from = ev.dataTransfer.getData("source");
+    let to = dragged.index;
+    reorder({ from, to });
+  };
+
+  // DISPATCH REORDER
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  const reorder = ({ from, to }) => {
+    let newList = [...list];
+    newList[from] = [newList[to], (newList[to] = newList[from])][0];
+
+    dispatch("reorder", newList);
+  };
+
+  export let list;
+  export let item;
+  export let key = "id";
+  let ListItem = item;
+</script>
+
+<style>
+  li {
+    border: 2px dotted transparent;
+    transition: border 0.1s linear;
+  }
+  .over {
+    border-color: rgba(48, 12, 200, 0.2);
+  }
+</style>
+
+<ul>
+  {#each list as item, index (item[key])}
+    <li
+      data-index={index}
+      data-id={JSON.stringify(item[key])}
+      draggable="true"
+      on:dragstart={start}
+      on:dragover={over}
+      on:dragleave={leave}
+      on:drop={drop}
+      in:receive={{ key: item[key] }}
+      out:send={{ key: item[key] }}
+      animate:flip={{ duration: 300 }}
+      class:over={item[key] === isOver}>
+      <slot {item} />
+    </li>
+  {/each}
+</ul>
